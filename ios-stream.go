c100c95a4df20cdbc908b895ddc538adc1f8cc1f
N.Shabanov <drunkencheetah@outlook.com>
2022-05-21 09:15:55 +0300

    a bit of cleanup

--- ios-stream.go
+++ ios-stream.go
@@ -16,10 +16,8 @@ import (
 	"github.com/pixiv/go-libjpeg/jpeg"
 )
 
-var maxFPS = 60
 var iOSImageChan = make(chan image.Image, 1)
 var initialImageChan = make(chan streamImageData, 1)
-var screenshotsChan = make(chan []byte)
 var lastImageBytes []byte
 var lastImage image.Image
 
@@ -53,7 +51,7 @@ func StreamIOS(w http.ResponseWriter, r *http.Request) {
 		Next: func() (image.Image, error) {
 			return <-iOSImageChan, nil
 		},
-		Options: &jpeg.EncoderOptions{Quality: 50, OptimizeCoding: false},
+		Options: &jpeg.EncoderOptions{Quality: 100, OptimizeCoding: false},
 	}
 	for {
 		// get handler new image from imageChan
@@ -89,28 +87,48 @@ func streamScreenshot(device ios.DeviceEntry) {
 	}
 }
 
-func scheduleNextScreenshot(timeInterval float64, timeStarted time.Time, device ios.DeviceEntry) {
-	//fmt.Printf("Current time for time elapsed calculation will be %v\n", time.Now().UnixNano())
-	//timeElapsed := time.Now().Sub(timeStarted)
-	//koleo := timeElapsed.Nanoseconds()
-	//fmt.Printf("This is time elapsed after calculation %v\n", koleo)
-	//nextTickDelta := timeInterval - float64(koleo)
-	//fmt.Printf("This is the next tick delta %v\n", nextTickDelta)
-	//s := fmt.Sprintf("%.2f", nextTickDelta)
-	//fmt.Println(s)
+func completeFunction(device ios.DeviceEntry) {
+	startTime := time.Now().UnixMilli()
+	screenshotrConn, err := screenshotr.New(device)
+	if err != nil {
+		panic(err.Error())
+	}
 
+	imageBytes, err := screenshotrConn.TakeScreenshot()
+	if err != nil {
+		screenshotrConn.Close()
+		return
+	}
+	screenshotrConn.Close()
+
+	var im image.Image
+
+	im, err = png.Decode(bytes.NewReader(imageBytes))
+	if err != nil {
+		fmt.Println("failed on png decode")
+		return
+	}
+
+	buf := new(bytes.Buffer)
+
+	err = jpeg2.Encode(buf, im, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	finalImage, err := jpeg.Decode(buf, &jpeg.DecoderOptions{})
+	if err != nil {
+		fmt.Println("Couldn't decode jpeg")
+	}
+	endTime := time.Now().UnixMilli()
+	imageStuff := streamImageData{Image: finalImage, StartTime: startTime, EndTime: endTime}
+	initialImageChan <- imageStuff
+	<-ch
+}
+
+func scheduleNextScreenshot(timeInterval float64, timeStarted time.Time, device ios.DeviceEntry) {
 	time.Sleep(100 * time.Millisecond)
 	go streamScreenshot(device)
-
-	// if nextTickDelta > 0 {
-	// 	time.Sleep(100 * time.Nanosecond)
-	// 	fmt.Println("inside next tick delta")
-	// 	//time.AfterFunc(time.Duration(nextTickDelta)*time.Nanosecond, f)
-	// 	go streamScreenshot(screenshotrService)
-	// } else {
-	// 	fmt.Println("outside next tick delta")
-	// 	go streamScreenshot(screenshotrService)
-	// }
 }
 
 func takeScreenshotToBytes(device ios.DeviceEntry) []byte {
@@ -125,53 +143,9 @@ func takeScreenshotToBytes(device ios.DeviceEntry) []byte {
 		return []byte{}
 	} else {
 		test.Close()
-		//fmt.Println("actually setting bytes")
 		return imageBytes
 	}
 
-}
-
-func completeFunction(device ios.DeviceEntry) {
-	startTime := time.Now().UnixMilli()
-	test, err := screenshotr.New(device)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	imageBytes, err := test.TakeScreenshot()
-	if err != nil {
-		test.Close()
-		return
-	}
-	test.Close()
-
-	var im image.Image
-
-	im, err = png.Decode(bytes.NewReader(imageBytes))
-	if err != nil {
-		//fmt.Println("NOT A PNG FILE")
-		fmt.Println("failed on png decode")
-		return
-	}
-
-	buf := new(bytes.Buffer)
-
-	err = jpeg2.Encode(buf, im, nil)
-	if err != nil {
-		//fmt.Println("Couldn't encode jpeg")
-		fmt.Println(err.Error())
-	}
-
-	finalImage, err := jpeg.Decode(buf, &jpeg.DecoderOptions{})
-	if err != nil {
-		fmt.Println("Couldn't decode jpeg")
-	}
-	//fmt.Println("New image")
-	//lastImage = finalImage
-	endTime := time.Now().UnixMilli()
-	imageStuff := streamImageData{Image: finalImage, StartTime: startTime, EndTime: endTime}
-	initialImageChan <- imageStuff
-	<-ch
 }
 
 func sendScreenshotCheck() {
@@ -187,7 +161,6 @@ func sendScreenshotCheck() {
 }
 
 func createJPEG(device ios.DeviceEntry) {
-	//fmt.Println("Goroutine start time is: " + time.Now().String())
 	imageBytes := takeScreenshotToBytes(device)
 	jpgDecode(pngDecode(imageBytes))
 }
@@ -195,12 +168,10 @@ func createJPEG(device ios.DeviceEntry) {
 func pngDecode(imageBytes []byte) image.Image {
 	res := bytes.Compare(lastImageBytes, imageBytes)
 	if res == 0 {
-		//fmt.Println("returning last image")
 		return lastImage
 	}
 	im, err := png.Decode(bytes.NewReader(imageBytes))
 	if err != nil {
-		//fmt.Println("NOT A PNG FILE")
 		return nil
 	}
 	return im
@@ -211,7 +182,6 @@ func jpgDecode(im image.Image) {
 
 	err := jpeg2.Encode(buf, im, nil)
 	if err != nil {
-		//fmt.Println("Couldn't encode jpeg")
 		fmt.Println(err.Error())
 	}
 
@@ -219,10 +189,6 @@ func jpgDecode(im image.Image) {
 	if err != nil {
 		fmt.Println("Couldn't decode jpeg")
 	}
-	//fmt.Println("New image")
 	lastImage = finalImage
-	//fmt.Println("Returned last frame time: " + strconv.Itoa(int(time.Now().UnixMilli())))
-	//fmt.Println("Goroutine last frame time is: " + time.Now().String())
-	fmt.Println("Sending screenshot at: " + time.Now().String())
 	iOSImageChan <- finalImage
 }
