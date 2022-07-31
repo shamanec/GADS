7612f25e261feda6c78f814cd02078d058163a97
N.Shabanov <drunkencheetah@outlook.com>
2022-07-31 14:15:38 +0300

    start removing unused code

--- main.go
+++ main.go
@@ -14,14 +14,6 @@ import (
 
 var project_log_file *os.File
 
-type AppiumConfig struct {
-	DevicesHost             string `json:"devices_host"`
-	SeleniumHubHost         string `json:"selenium_hub_host"`
-	SeleniumHubPort         string `json:"selenium_hub_port"`
-	SeleniumHubProtocolType string `json:"selenium_hub_protocol_type"`
-	WDABundleID             string `json:"wda_bundle_id"`
-}
-
 type DeviceConfig struct {
 	OS                  string `json:"os"`
 	AppiumPort          int    `json:"appium_port"`
@@ -39,17 +31,11 @@ type DeviceConfig struct {
 }
 
 type EnvConfig struct {
-	SudoPassword         string   `json:"sudo_password"`
-	ConnectSeleniumGrid  bool     `json:"connect_selenium_grid"`
-	SupervisionPassword  string   `json:"supervision_password"`
-	ContainerizedUsbmuxd string   `json:"containerized_usbmuxd"`
-	DeviceProviders      []string `json:"device_providers"`
+	DeviceProviders []string `json:"device_providers"`
 }
 
 type ConfigJsonData struct {
-	AppiumConfig AppiumConfig   `json:"appium-config"`
-	EnvConfig    EnvConfig      `json:"env-config"`
-	DeviceConfig []DeviceConfig `json:"devices-config"`
+	EnvConfig EnvConfig `json:"env-config"`
 }
 
 // Load the initial page
@@ -85,21 +71,8 @@ func handleRequests() {
 		httpSwagger.DomID("#swagger-ui"),
 	))
 
-	// General containers endpoints
-	myRouter.HandleFunc("/containers/{container_id}/restart", RestartContainer).Methods("POST")
-	myRouter.HandleFunc("/containers/{container_id}/remove", RemoveContainer).Methods("POST")
-	myRouter.HandleFunc("/containers/{container_id}/logs", GetContainerLogs).Methods("GET")
-
-	// Configuration endpoints
-	myRouter.HandleFunc("/configuration/build-image/{image_type}", BuildDockerImage).Methods("POST")
-	myRouter.HandleFunc("/configuration/remove-image/{image_type}", RemoveDockerImage).Methods("POST")
-	myRouter.HandleFunc("/configuration/update-config", UpdateProjectConfigHandler).Methods("PUT")
 	myRouter.HandleFunc("/configuration/upload-wda", UploadWDA).Methods("POST")
 	myRouter.HandleFunc("/configuration/upload-app", UploadApp).Methods("POST")
-
-	// Devices endpoints
-	myRouter.HandleFunc("/device-logs/{log_type}/{device_udid}", GetDeviceLogs).Methods("GET")
-	myRouter.HandleFunc("/devices/device-control", GetDeviceControlInfo).Methods("GET")
 	myRouter.HandleFunc("/devices/available-devices", GetAvailableDevicesInfo).Methods("GET")
 
 	// Logs
@@ -108,11 +81,7 @@ func handleRequests() {
 	// Asset endpoints
 	myRouter.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
 
-	// Page loads
-	myRouter.HandleFunc("/device-containers", LoadDeviceContainers)
-	myRouter.HandleFunc("/refresh-device-containers", RefreshDeviceContainers)
 	myRouter.HandleFunc("/logs", GetLogsPage)
-	myRouter.HandleFunc("/device-control", GetDeviceControlPage)
 	myRouter.HandleFunc("/", GetInitialPage)
 	myRouter.HandleFunc("/devices", LoadAvailableDevices)
 	myRouter.HandleFunc("/refresh-available-devices", RefreshAvailableDevices)
