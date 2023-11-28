22302a412f76c005732048cdafcf9f32483aed70
shamanec <drunkencheetah@outlook.com>
2023-11-28 17:25:00 +0200

    fix streaming

--- gads-ui/src/components/DeviceControl/StreamCanvas.js
+++ gads-ui/src/components/DeviceControl/StreamCanvas.js
@@ -25,42 +25,19 @@ export default function StreamCanvas({ deviceData }) {
 
         if (deviceData.os === 'ios') {
             streamSocket = new WebSocket(`ws://${process.env.REACT_APP_GADS_BACKEND_HOST}/device/${deviceData.udid}/ios-stream`);
-
-            streamSocket.onmessage = (message) => {
-                let imgElement = document.getElementById('image-stream')
-
-                streamSocket.onmessage = function (event) {
-                    const imageURL = URL.createObjectURL(event.data);
-                    imgElement.src = imageURL
-
-                    imgElement.onload = () => {
-                        URL.revokeObjectURL(imageURL);
-                    };
-                }
-            }
         } else {
             streamSocket = new WebSocket(`ws://${process.env.REACT_APP_GADS_BACKEND_HOST}/device/${deviceData.udid}/android-stream`);
-            streamSocket.binaryType = 'arraybuffer'
+        }
 
-            let imgElement = document.getElementById('image-stream')
+        let imgElement = document.getElementById('image-stream')
+        streamSocket.onmessage = (message) => {
             streamSocket.onmessage = function (event) {
-                // Get the message data
-                const data = event.data
-                // Get the first 4 bytes of the message to Int
-                // To determing the message type - info or image
-                const messageType = new DataView(data.slice(0, 4)).getInt32(0, false)
+                const imageURL = URL.createObjectURL(event.data);
+                imgElement.src = imageURL
 
-                // If message type is 2(Image)
-                if (messageType == 2) {
-                    // Create an image URL
-                    const imageURL = URL.createObjectURL(new Blob([data.slice(4)]))
-                    // Set the image in the image element to create a stream
-                    imgElement.src = imageURL
-
-                    imgElement.onload = () => {
-                        URL.revokeObjectURL(imageURL);
-                    };
-                }
+                imgElement.onload = () => {
+                    URL.revokeObjectURL(imageURL);
+                };
             }
         }
 
