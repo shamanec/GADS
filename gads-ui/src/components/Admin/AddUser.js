7d6d81091c6665782f07b8c67081c36547e5c44f
shamanec <drunkencheetah@outlook.com>
2023-12-04 22:09:43 +0200

    improve add device form

--- gads-ui/src/components/Admin/AddUser.js
+++ gads-ui/src/components/Admin/AddUser.js
@@ -3,13 +3,15 @@ import TextField from '@mui/material/TextField'
 import { useState } from 'react';
 import { Button } from '@mui/material';
 import { Alert } from '@mui/material';
+import MenuItem from '@mui/material/MenuItem';
 import './AddUser.css'
+import Select from '@mui/material/Select';
 
 
 export default function AddUser() {
     const [username, setUsername] = useState()
     const [password, setPassword] = useState()
-    const [role, setRole] = useState()
+    const [role, setRole] = useState('user')
     const [email, setEmail] = useState()
     const [showAlert, setShowAlert] = useState(false)
     const [alertText, setAlertText] = useState()
@@ -49,57 +51,55 @@ export default function AddUser() {
     }
 
     return (
-
-        <form onSubmit={handleAddUser}>
-            <Stack className='add-user' alignItems="center" justifyContent="center" spacing={2}>
-                <label autoCom>
-                    <TextField
-                        onChange={e => setUsername(e.target.value)}
-                        label="username"
-                        required
-                        id="outlined-required"
-                        autoComplete='off'
-                    />
-                </label>
-                <label style={{}}>
-                    <TextField
-                        onChange={e => setPassword(e.target.value)}
-                        label="password"
-                        required
-                        id="outlined-required"
-                        autoComplete='off'
-                    />
-                </label>
-                <label style={{}}>
-                    <TextField
-                        onChange={e => setRole(e.target.value)}
-                        label="role"
-                        required
-                        id="outlined-required"
-                        autoComplete='off'
-                    />
-                </label>
-                <label style={{}}>
-                    <TextField
-                        onChange={e => setEmail(e.target.value)}
-                        label="email"
-                        required
-                        id="outlined-required"
-                        autoComplete='off'
-                    />
-                </label>
-                <div>
-                    <Button
-                        variant="contained"
-                        type="submit"
-                        style={{}}
-                    >Add user</Button>
-                </div>
-
-                {showAlert && <Alert severity="error">{alertText}</Alert>}
-            </Stack >
-        </form >
-
-
+        <div>
+            <form onSubmit={handleAddUser}>
+                <Stack className='add-user' alignItems="center" justifyContent="center" spacing={2}>
+                    <label autoCom>
+                        <TextField
+                            onChange={e => setUsername(e.target.value)}
+                            label="username"
+                            required
+                            id="outlined-required"
+                            autoComplete='off'
+                        />
+                    </label>
+                    <label style={{}}>
+                        <TextField
+                            onChange={e => setPassword(e.target.value)}
+                            label="password"
+                            required
+                            id="outlined-required"
+                            autoComplete='off'
+                        />
+                    </label>
+                    <Select
+                        defaultValue='user'
+                        value={role}
+                        onChange={(event) => setRole(event.target.value)}
+                        style={{ width: '100px', marginTop: "20px" }}
+                    >
+                        <MenuItem value='user'>User</MenuItem>
+                        <MenuItem value='admin'>Admin</MenuItem>
+                    </Select>
+                    <label style={{}}>
+                        <TextField
+                            onChange={e => setEmail(e.target.value)}
+                            label="email"
+                            required
+                            id="outlined-required"
+                            autoComplete='off'
+                        />
+                    </label>
+                    <div>
+                        <Button
+                            variant="contained"
+                            type="submit"
+                            style={{}}
+                        >Add user</Button>
+                    </div>
+                    {showAlert && <Alert severity="error">{alertText}</Alert>}
+                </Stack >
+            </form >
+        </div>
     )
 }
