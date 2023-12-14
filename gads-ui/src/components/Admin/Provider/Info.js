7bc899cab95a270f2c9b7f1a46b7e320b4434397
shamanec <drunkencheetah@outlook.com>
2023-12-14 17:10:46 +0200

    cleanup

--- gads-ui/src/components/Admin/Provider/Info.js
+++ gads-ui/src/components/Admin/Provider/Info.js
@@ -8,15 +8,12 @@ import Box from '@mui/material/Box';
 export default function Info({ info }) {
     return (
         <Box
-            sx={{ height: '100px', maxWidth: '200px', background: 'white', marginTop: '10px', borderRadius: '10px', marginLeft: '10px' }}
+            sx={{ height: '100px', maxWidth: '200px', background: 'white', borderRadius: '10px', margin: '10px' }}
         >
             <Tooltip title='Configured devices' placement='left' leaveDelay={0}>
                 <ListItem>
                     <ListItemIcon>
-
                         <SmartphoneIcon />
-
-
                     </ListItemIcon>
                     <ListItemText
                         primary={info.devices}
