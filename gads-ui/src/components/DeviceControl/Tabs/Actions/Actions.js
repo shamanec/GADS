f58a58137118d6f45d7579e1623705ddb124faa7
shamanec <drunkencheetah@outlook.com>
2023-12-26 17:40:20 +0200

    show an error

--- gads-ui/src/components/DeviceControl/Tabs/Actions/Actions.js
+++ gads-ui/src/components/DeviceControl/Tabs/Actions/Actions.js
@@ -1,17 +1,14 @@
 import { useContext, useState } from "react";
 import Apps from "./Apps/Apps";
-import { Box, CircularProgress, Stack, TextField } from "@mui/material";
+import { Alert, Box, CircularProgress, Stack, TextField } from "@mui/material";
 import axios from "axios";
 import { Auth } from "../../../../contexts/Auth";
-import { useDialog } from "../../SessionDialogContext";
 
 export default function Actions({ deviceData }) {
     return (
         <Box marginTop='10px'>
             <Stack>
-                <Apps deviceData={deviceData}>
-
-                </Apps>
+                <Apps deviceData={deviceData}></Apps>
                 <TypeText deviceData={deviceData}></TypeText>
             </Stack>
         </Box>
@@ -19,9 +16,9 @@ export default function Actions({ deviceData }) {
 }
 
 function TypeText({ deviceData }) {
-    const { setDialog } = useDialog()
     const [isTyping, setIsTyping] = useState(false)
     const [authToken, , logout] = useContext(Auth)
+    const [showError, setShowError] = useState(false)
 
     function handleEnter(event) {
         // If currently typing text through the API with Appium do not allow typing in input box
@@ -35,9 +32,16 @@ function TypeText({ deviceData }) {
         }
     }
 
+    function handleShowError() {
+        setShowError(false)
+        setShowError(true)
+        setTimeout(() => {
+            setShowError(false)
+        }, 3000)
+    }
+
     function handleType(text) {
         setIsTyping(true)
-
         let json = `{"text": "${text}"}`
 
         let url = `/device/${deviceData.Device.udid}/typeText`
@@ -53,19 +57,19 @@ function TypeText({ deviceData }) {
                         return
                     }
                     if (error.response.status === 404) {
-                        // setDialog(true)
                     }
+                    handleShowError()
                 }
             })
             .finally(() => {
                 setTimeout(() => {
                     setIsTyping(false)
-                }, 500)
+                }, 1000)
             })
     }
 
     return (
-        <Box style={{ backgroundColor: 'white', width: '600px', borderRadius: '10px', marginTop: '5px' }}>
+        <Box style={{ backgroundColor: 'white', width: '600px', marginTop: '5px', height: '155px' }}>
             <div style={{ marginLeft: '10px', marginTop: '5px' }}>Make sure you've selected input element in the app</div>
             <Box style={{ display: 'flex', alignItems: 'center', marginLeft: '10px', marginBottom: '10px' }}>
                 <TextField
@@ -73,12 +77,15 @@ function TypeText({ deviceData }) {
                     label="Type something and press Enter"
                     variant="outlined"
                     onKeyUp={(event) => handleEnter(event)}
-                    style={{ backgroundColor: 'white', marginTop: '15px', width: '80%', borderBottomLeftRadius: '5px', borderBottomRightRadius: '5px' }}
+                    style={{ backgroundColor: 'white', marginTop: '15px', width: '80%' }}
                 />
                 {isTyping &&
                     <CircularProgress id='progress-indicator' size={45} />
                 }
             </Box>
+            {showError &&
+                <Alert severity="error">Error typing or no active input element selected</Alert>
+            }
         </Box>
     )
 }
