ab132724d654b6b2379e65ed9f77c12b4994db9b
shamanec <drunkencheetah@outlook.com>
2024-06-10 19:10:32 +0300

    take another approach

--- hub/gads-ui/src/Gads.js
+++ hub/gads-ui/src/Gads.js
@@ -8,33 +8,33 @@ import {useContext, useEffect} from 'react'
 import { Auth } from './contexts/Auth'
 import AdminDashboard from './components/Admin/AdminDashboard'
 import axiosInterceptor from './services/axiosInterceptor'
-import versionData from './version.json'
 
 function Gads() {
-  const {authToken, logout} = useContext(Auth)
-  // Set the logout function from the Auth context on the axiosInterceptor to automatically logout on each 401
-  axiosInterceptor(logout)
+    const {authToken, logout} = useContext(Auth)
+    // Set the logout function from the Auth context on the axiosInterceptor to automatically logout on each 401
+    axiosInterceptor(logout)
 
     useEffect(() => {
         localStorage.removeItem('gadsVersion')
-        localStorage.setItem('gadsVersion', versionData.version)
+        let version = process.env.REACT_APP_VERSION || 'unknown'
+        localStorage.setItem('gadsVersion', version)
     }, [])
 
-  if (!authToken) {
-    return <Login />
-  }
+    if (!authToken) {
+        return <Login/>
+    }
 
-  return (
-    <div style={{ backgroundColor: "#f4e6cd", height: "100%" }}>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Navigate to="/devices" />} />
-        <Route path="/devices" element={<DeviceSelection />} />
-        <Route path="/devices/control/:id" element={<DeviceControl />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-      </Routes>
-    </div>
-  )
+    return (
+        <div style={{backgroundColor: "#f4e6cd", height: "100%"}}>
+            <NavBar/>
+            <Routes>
+                <Route path="/" element={<Navigate to="/devices"/>}/>
+                <Route path="/devices" element={<DeviceSelection/>}/>
+                <Route path="/devices/control/:id" element={<DeviceControl/>}/>
+                <Route path="/admin" element={<AdminDashboard/>}/>
+            </Routes>
+        </div>
+    )
 }
 
 export default Gads
