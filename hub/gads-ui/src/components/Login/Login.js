66b9a463b21dbe821ccecf16bb5d2f664dd03aeb
shamanec <drunkencheetah@outlook.com>
2024-07-09 20:27:15 +0300

    make login component consistent

--- hub/gads-ui/src/components/Login/Login.js
+++ hub/gads-ui/src/components/Login/Login.js
@@ -1,18 +1,18 @@
 import { useState, useContext } from "react"
 import { useNavigate } from "react-router-dom"
 import { Auth } from "../../contexts/Auth"
-import './Login.css'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
 import Alert from '@mui/material/Alert'
 import { api } from '../../services/api.js'
+import {Box, Stack} from "@mui/material";
 
 export default function Login() {
-    const [username, setUsername] = useState()
-    const [password, setPassword] = useState()
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
     const { login } = useContext(Auth)
     const [showAlert, setShowAlert] = useState(false)
-    const [alertText, setAlertText] = useState()
+    const [alertText, setAlertText] = useState('')
     const navigate = useNavigate()
 
     function toggleAlert(message) {
@@ -23,110 +23,117 @@ export default function Login() {
     function handleLogin(event) {
         event.preventDefault()
 
-        let url = `/authenticate`
-
         const loginData = {
             username: username,
             password: password,
         }
 
+        let url = `/authenticate`
         api.post(url, loginData)
             .then(response => {
-                if (response.status !== 200) {
-                    toggleAlert(response.data.error);
-                    throw new Error(response.data.error)
-                } else {
-                    return response.data
-                }
+                return response.data
             })
             .then(json => {
                 const sessionID = json.sessionID
                 login(sessionID, json.username, json.role)
                 navigate("/devices")
             })
-            .catch(e => {
-                console.log("Login failed")
-                console.log(e)
+            .catch((e) => {
+                if (e.response.status === 401) {
+                    toggleAlert('Invalid credentials');
+                } else {
+                    toggleAlert('Something went wrong')
+                }
+            })
+            .finally(() => {
+                setTimeout(() => {
+                    setShowAlert(false)
+                }, 3000)
             })
     }
 
     let gadsVersion = localStorage.getItem('gadsVersion') || 'unknown'
 
     return (
-        <div className="top-wrapper">
-            <div className="fancy-wrapper">
-                <div id="funky-div">
-                </div>
-                <div className="login-wrapper">
+        <Box style={{ height: '100vh', width: '100vw', justifyContent: 'center', alignItems: 'center', display: 'flex', backgroundColor: '#f4e6cd'}}>
+            <Box
+                style={{
+                    width: '30%',
+                    height: '500px',
+                    backgroundColor: '#9ba984',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    borderRadius: '10px'
+                }}
+            >
+                <Box
+                    style={{
+                        background: 'linear-gradient(62deg, rgba(38,199,127,1) 0%, rgba(200,137,0,1) 100%)',
+                        width: '50%',
+                        borderTopLeftRadius: '10px',
+                        borderBottomLeftRadius: '10px'
+                    }}
+                ></Box>
+                <Box
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        width: '50%'
+                    }}
+                >
                     <img
                         src="./images/gads.png"
                         style={{
-                            width: '50%'
+                            width: '50%',
+                            marginBottom: '20px'
                         }}
                     ></img>
-                    <h2>Please log in</h2>
-                    <form
-                        onSubmit={handleLogin}
-                        style={{
-                            display: "flex",
-                            flexDirection: "column"
-                        }}
-                    >
-                        <label>
+                    <form onSubmit={handleLogin}>
+                        <Stack spacing={2}>
                             <TextField
-                                onChange={e => setUsername(e.target.value)}
-                                label="Username"
                                 required
-                                id="outlined-required"
-                                style={{color: "#9ba984"}}
-                                sx={{
-                                    input: {
-                                        background: "#9ba984"
-                                    }
-                                }}
+                                label='Username'
+                                autoComplete='off'
+                                size='small'
+                                onChange={(e) => setUsername(e.target.value)}
                             />
-                        </label>
-                        <label
-                            style={{
-                                marginTop: "20px",
-                                marginBottom: "20px"
-                            }}
-                        >
                             <TextField
-                                onChange={e => setPassword(e.target.value)}
-                                type="password"
-                                label="Password"
                                 required
-                                id="outlined-required"
-                                style={{color: "#9ba984"}}
+                                label='Password'
+                                autoComplete='off'
+                                size='small'
+                                type='password'
+                                onChange={(e) => setPassword(e.target.value)}
                             />
-                        </label>
-                        <div>
                             <Button
-                                variant="contained"
-                                type="submit"
+                                variant='contained'
+                                type='submit'
                                 style={{
-                                    marginBottom: "5px",
-                                    backgroundColor: "#2f3b26",
-                                    color: "#9ba984",
-                                    fontWeight: "bold"
+                                    backgroundColor: '#2f3b26',
+                                    color: '#f4e6cd',
+                                    fontWeight: 'bold',
+                                    boxShadow: 'none',
+                                    height: '40px'
                                 }}
-                            >Log in</Button>
-                        </div>
-                        {showAlert && <Alert severity="error">{alertText}</Alert>}
+                            >Log In</Button>
+                            <p
+                                style={{
+                                    width: '100%',
+                                    marginRight: '20px',
+                                    textAlign: 'right',
+                                    fontWeight: 'bold',
+                                    color: '#2f3b26',
+                                }}
+                            >{gadsVersion.startsWith('v') ? gadsVersion : "DEV"}
+                            </p>
+                            <Alert severity="error" style={{padding: '2px 4px', visibility: showAlert ? 'visible' : 'hidden',}}>{alertText}</Alert>
+                        </Stack>
                     </form>
-                    <p
-                        style={{
-                            width: '100%',
-                            marginRight: '20px',
-                            textAlign: 'right',
-                            fontWeight: 'bold',
-                            color: '#2f3b26'
-                        }}
-                    >{gadsVersion.startsWith('v') ? gadsVersion : "DEV"}
-                    </p>
-                </div>
-            </div>
-        </div>
+                </Box>
+
+            </Box>
+        </Box>
     )
 }
