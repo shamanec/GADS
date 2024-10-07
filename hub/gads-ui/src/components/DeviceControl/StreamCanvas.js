57baec182c83f3ce1ea2ce651ca4f37a12792ce0
shamanec <drunkencheetah@outlook.com>
2024-10-07 14:01:16 +0300

    improve landscape interactions

--- hub/gads-ui/src/components/DeviceControl/StreamCanvas.js
+++ hub/gads-ui/src/components/DeviceControl/StreamCanvas.js
@@ -256,7 +256,7 @@ function Canvas({ authToken, logout, streamData, setDialog }) {
         // if x2 < x1*0.9 - it is probably a swipe right to left
         // if y2 < y1*0.9 - it is probably a swipe bottom to top
         // if y2 > y1*1.1 - it is probably a swipe top to bottom
-        if (mouseEventsTimeDiff > 500 || coord2[0] > coord1[0] * 1.1 || coord2[0] < coord1[0] * 0.9 || coord2[1] < coord1[1] * 0.9 || coord2[1] > coord1[1] * 1.1) {
+        if (mouseEventsTimeDiff > 500 && coord2[0] > coord1[0] * 1.1 || coord2[0] < coord1[0] * 0.9 || coord2[1] < coord1[1] * 0.9 || coord2[1] > coord1[1] * 1.1) {
             swipeCoordinates(authToken, logout, coord1, coord2, streamData, setDialog)
         } else if (mouseEventsTimeDiff < 500) {
             tapCoordinates(authToken, logout, coord1, streamData, setDialog)
@@ -291,7 +291,6 @@ function Stream({ canvasWidth, canvasHeight, streamUrl }) {
 
 // tap using coordinates
 function tapCoordinates(authToken, logout, pos, streamData, setDialog) {
-    console.log("tapping")
     // set initial x and y tap coordinates
     let x = pos[0]
     let y = pos[1]
@@ -348,15 +347,34 @@ function touchAndHoldCoordinates(authToken, logout, pos, streamData, setDialog) 
     let x = pos[0]
     let y = pos[1]
 
-    // if the stream height 
-    if (streamData.canvasHeight != streamData.deviceY) {
-        x = (x / streamData.canvasWidth) * streamData.deviceX
-        y = (y / streamData.canvasHeight) * streamData.deviceY
+    let finalX = (x / streamData.canvasWidth) * streamData.deviceX
+    let finalY = (y / streamData.canvasHeight) * streamData.deviceY
+
+    // If its portrait we keep the x and y as is
+    if (!streamData.isPortrait) {
+        // If its landscape
+        // And its Android we still keep the x and y as is because for Android Appium does the coordinates are actually corresponding to the view
+        // If we are in portrait X is X and Y is Y and when we are in landscape its the same
+        if (streamData.device_os === 'android') {
+            finalX = (x / streamData.canvasHeight) * streamData.deviceX
+            finalY = (y / streamData.canvasWidth) * streamData.deviceY
+        }
+        if (streamData.device_os === 'ios') {
+            if (streamData.uses_custom_wda) {
+                finalX = streamData.deviceX - ((y / streamData.canvasHeight) * streamData.deviceX)
+                finalY = (x / streamData.canvasWidth) * streamData.deviceY
+            } else {
+                // On normal WDA in landscape X corresponds to the canvas width but it is actually the device's height
+                finalX = (x / streamData.canvasWidth) * streamData.deviceY
+                // Y corresponds to the canvas height but it is actually the device's width
+                finalY = (y / streamData.canvasHeight) * streamData.deviceX
+            }
+        }
     }
 
     let jsonData = JSON.stringify({
-        "x": x,
-        "y": y
+        "x": finalX,
+        "y": finalY
     })
 
     let deviceURL = `/device/${streamData.udid}`
