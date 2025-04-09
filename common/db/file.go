004ca8de38a4fdd055cf7a7e350d6b01548c6d0e
shamanec <drunkencheetah@outlook.com>
2025-04-09 16:41:53 +0300

    update db file upload and download to new mongo code structure

--- common/db/file.go
+++ common/db/file.go
@@ -2,8 +2,10 @@ package db
 
 import (
 	"GADS/common/models"
+	"bytes"
 	"fmt"
 	"io"
+	"os"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -17,7 +19,7 @@ func (m *MongoStore) GetFiles() ([]models.DBFile, error) {
 
 func (m *MongoStore) UploadFile(file io.Reader, fileName string, force bool) error {
 	bucket, err := gridfs.NewBucket(m.GetDefaultDatabase(), nil)
-	filter := bson.D{{"filename", fileName}}
+	filter := bson.D{{Key: "filename", Value: fileName}}
 
 	cursor, err := bucket.Find(filter)
 	if err != nil {
@@ -66,4 +68,74 @@ func (m *MongoStore) UploadFile(file io.Reader, fileName string, force bool) err
 		}
 		return nil
 	}
+}
+
+func (m *MongoStore) DownloadFile(fileName, downloadPath string) error {
+	bucket, err := gridfs.NewBucket(m.GetDefaultDatabase(), nil)
+	if err != nil {
+		return err
+	}
+
+	// Create a filter and search the bucket for the WebDriverAgent.ipa file
+	filter := bson.D{{Key: "filename", Value: fileName}}
+	cursor, err := bucket.Find(filter)
+	if err != nil {
+		return fmt.Errorf("Failed to get cursor from DB - %s", err)
+	}
+
+	// Try to get the found files from the cursor
+	type gridfsFile struct {
+		Name string `bson:"filename"`
+		ID   string `bson:"_id"`
+	}
+	var foundFiles []gridfsFile
+	err = cursor.All(GlobalMongoStore.Ctx, &foundFiles)
+	if err != nil {
+		return fmt.Errorf("Failed to get files from DB cursor - %s", err)
+	}
+
+	// If no found files
+	if len(foundFiles) == 0 {
+		return fmt.Errorf("%s is not present in MongoDB, have you uploaded it?", fileName)
+	}
+
+	// If more than 1 found file
+	if len(foundFiles) > 1 {
+		fmt.Printf("There is more than one %s file in MongoDB, will download the first one!\n", fileName)
+	}
+
+	// Create the filepath and remove the supervision profile file if present
+	filePath := fmt.Sprintf("%s/%s", downloadPath, fileName)
+	err = os.Remove(filePath)
+	if err != nil {
+		fmt.Printf("There is no %s file located at `%s`, nothing to remove\n", fileName, filePath)
+	}
+
+	// Get the ObjectID from the file ID in Mongo
+	id, err := primitive.ObjectIDFromHex(foundFiles[0].ID)
+	downloadStream, err := bucket.OpenDownloadStream(id)
+	if err != nil {
+		return fmt.Errorf("Failed to open download stream from the GridFS bucket - %s", err)
+	}
+
+	// Create a new buffer and read the download stream to it
+	fileBuffer := bytes.NewBuffer(nil)
+	if _, err := io.Copy(fileBuffer, downloadStream); err != nil {
+		return fmt.Errorf("Failed to copy download stream to the bytes buffer - %s", err)
+	}
+
+	// Create the file on the provider host
+	actualFile, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("Failed to create file with path `%s` - %s", filePath, err)
+	}
+	defer actualFile.Close()
+
+	// Write the file contents to the file
+	_, err = actualFile.Write(fileBuffer.Bytes())
+	if err != nil {
+		return fmt.Errorf("Failed to write byte to file with path `%s` - %s", filePath, err)
+	}
+
+	return nil
 }
