07cdf78439e388de9d07b4125618501a370f9c78
shamanec <drunkencheetah@outlook.com>
2025-07-10 14:06:33 +0300

    fix running appium with inline json config

--- common/models/models.go
+++ common/models/models.go
@@ -235,8 +235,9 @@ type AndroidFileNode struct {
 }
 
 type AppiumPluginConfiguration struct {
-	ProviderUrl  string                   `json:"providerUrl"`
-	DeviceConfig AppiumPluginDeviceConfig `json:"configuration"`
+	ProviderUrl       string                   `json:"providerUrl"`
+	DeviceConfig      AppiumPluginDeviceConfig `json:"configuration"`
+	HeartBeatInterval string                   `json:"heartbeatIntervalMs"`
 }
 
 type AppiumPluginDeviceConfig struct {
