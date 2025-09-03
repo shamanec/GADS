4ce67ad416168b06806bfde21c063bdaf8ddf547
shamanec <drunkencheetah@outlook.com>
2025-09-03 09:11:52 +0300

    remove nonexisting commands from classifier

--- appium-plugin/src/utils/classifier.js
+++ appium-plugin/src/utils/classifier.js
@@ -10,13 +10,13 @@ const log = logger.getLogger('GADS');
  */
 export function classify(commandName, args) {
     log.info(`Classifying command ${commandName}`);
-    
+
     switch (commandName) {
-        case 'click': 
+        case 'click':
             return { action: 'Tap' };
-        case 'setValue': 
+        case 'setValue':
             return { action: 'Type' };
-        case 'performActions': 
+        case 'performActions':
             return { action: 'Gesture' };
         case 'executeScript': {
             const script = args?.[0];
@@ -25,98 +25,88 @@ export function classify(commandName, args) {
             }
             return null;
         }
-        case 'findElement': 
+        case 'findElement':
             return { action: 'Find Element' };
-        case 'findElements': 
+        case 'findElements':
             return { action: 'Find Elements' };
-        case 'getClipboard': 
+        case 'getClipboard':
             return { action: 'Get Clipboard' };
-        
+
         // Element Interaction Commands
-        case 'getText': 
+        case 'getText':
             return { action: 'Get Text' };
-        case 'getAttribute': 
+        case 'getAttribute':
             return { action: 'Get Attribute' };
-        case 'sendKeys': 
+        case 'sendKeys':
             return { action: 'Send Keys' };
-        case 'clear': 
+        case 'clear':
             return { action: 'Clear Field' };
-        case 'isDisplayed': 
+        case 'isDisplayed':
             return { action: 'Check Visibility' };
-        case 'isEnabled': 
+        case 'isEnabled':
             return { action: 'Check Enabled State' };
-        case 'isSelected': 
+        case 'isSelected':
             return { action: 'Check Selection State' };
-        case 'getRect': 
+        case 'getRect':
             return { action: 'Get Element Bounds' };
-        case 'getSize': 
+        case 'getSize':
             return { action: 'Get Element Size' };
-        case 'getLocation': 
+        case 'getLocation':
             return { action: 'Get Element Position' };
-        
+
         // Navigation & App Control Commands
-        case 'back': 
+        case 'back':
             return { action: 'Navigate Back' };
-        case 'forward': 
+        case 'forward':
             return { action: 'Navigate Forward' };
-        case 'refresh': 
+        case 'refresh':
             return { action: 'Refresh Page' };
-        case 'getCurrentUrl': 
+        case 'getCurrentUrl':
             return { action: 'Get Current URL' };
-        case 'getTitle': 
+        case 'getTitle':
             return { action: 'Get Page Title' };
-        case 'quit': 
+        case 'quit':
             return { action: 'Quit Session' };
-        
+
         // Device Actions
-        case 'shake': 
+        case 'shake':
             return { action: 'Shake Device' };
-        case 'lock': 
+        case 'lock':
             return { action: 'Lock Device' };
-        case 'unlock': 
+        case 'unlock':
             return { action: 'Unlock Device' };
-        case 'pressKeyCode': 
+        case 'pressKeyCode':
             return { action: 'Press Key' };
-        case 'longPressKeyCode': 
+        case 'longPressKeyCode':
             return { action: 'Long Press Key' };
-        case 'hideKeyboard': 
+        case 'hideKeyboard':
             return { action: 'Hide Keyboard' };
-        case 'isKeyboardShown': 
+        case 'isKeyboardShown':
             return { action: 'Check Keyboard State' };
-        
-        // Advanced Actions
-        case 'swipe': 
-            return { action: 'Swipe Gesture' };
-        case 'scroll': 
-            return { action: 'Scroll Action' };
-        case 'pinch': 
-            return { action: 'Pinch Gesture' };
-        case 'zoom': 
-            return { action: 'Zoom Gesture' };
-        case 'touchAction': 
-            return { action: 'Touch Action' };
-        case 'multiAction': 
-            return { action: 'Multi Touch' };
-        
+
+        // Advanced Actions (W3C Actions API)
+        case 'performActions':
+            return { action: 'Perform Actions' };
+
         // App Management Commands
-        case 'activateApp': 
+        case 'activateApp':
             return { action: 'Activate App' };
-        case 'terminateApp': 
+        case 'terminateApp':
             return { action: 'Terminate App' };
-        case 'installApp': 
+        case 'installApp':
             return { action: 'Install App' };
-        case 'removeApp': 
+        case 'removeApp':
             return { action: 'Remove App' };
-        case 'backgroundApp': 
+        case 'backgroundApp':
             return { action: 'Background App' };
-        case 'queryAppState': 
+        case 'queryAppState':
             return { action: 'Query App State' };
-        case 'getCurrentPackage': 
+        case 'getCurrentPackage':
             return { action: 'Get Current Package' };
-        case 'startActivity': 
+        case 'startActivity':
             return { action: 'Start Activity' };
-        
-        default: 
-            return null;
+
+        default:
+            return { action: 'Other' };
     }
 }
